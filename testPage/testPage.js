a9eec95f7b1f34a2dbc1f5f63a9e0839a30581f4
Ezra-H <44772185+Ezra-H@users.noreply.github.com>
2020-06-20 10:49:25 +0800

    登录的wx.login函数

--- testPage/testPage.js
+++ testPage/testPage.js
@@ -127,6 +127,49 @@ Page({
     })
   },
   
+  wxlogin:function (e) {
+    wx.login({
+      success: resp => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(resp);
+        var that = this;
+        // 获取用户信息
+        wx.getSetting({
+          success: res => {
+            if (res.authSetting["scope.userInfo"]) {
+              // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+              wx.getUserInfo({
+                success: userResult => {
+                  var platUserInfoMap = {}
+                  platUserInfoMap["encryptedData"] = userResult.encryptedData;
+                  platUserInfoMap["iv"] = userResult.iv;
+                  wx.request({
+                    url: "http://127.0.0.1:5000/wx_login/login",
+                    data: { 
+                      platCode: resp.code,
+                      platUserInfoMap: platUserInfoMap,
+                    },
+                    header: { "Content-Type": "application/json"},
+                    method: "POST",
+                    dataType:"json",
+                  success: function (res) {
+                    console.log(res)
+                    wx.setStorageSync("userinfo", res.userinfo) //设置本地缓存
+                    that.setData({
+                      openid: res.data["result"]['wxid']
+                    });
+                  },
+                  fail: function (err) { },//请求失败
+                  complete: function () { }//请求完成后执行的函数
+                  })
+                }
+              })
+          } 
+          }
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
